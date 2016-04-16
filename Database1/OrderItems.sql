4407156077c432f9ddd8efd13d30f28225f2b3df
rakraj <rak.iloveu@gmail.com>
2016-04-16 13:05:02 -0400

    initial commit

---
+++ Database1/OrderItems.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[OrderItems]
+(
+	[Id] INT NOT NULL , 
+    [OrderId] INT NULL, 
+    [ItemId] INT NULL, 
+    [ItemQuantity] INT NULL, 
+    CONSTRAINT [PK_OrderItems] PRIMARY KEY ([Id]), 
+    CONSTRAINT [FK_OrderItems_ToOrders] FOREIGN KEY ([OrderId]) REFERENCES [Orders]([Id])
+)
---
+++ Database1/Orders.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	[Id] INT NOT NULL, 
+    [OrderDt] DATETIME NULL, 
+    [OrderQuantity] INT NULL, 
+    [OrderTotal] DECIMAL(18, 2) NULL, 
+    [ShippingDt] DATETIME NULL, 
+    [ShippingMethod] VARCHAR(50) NULL, 
+    [ShippingTotal] DECIMAL(18, 2) NULL, 
+    CONSTRAINT [PK_Orders] PRIMARY KEY ([Id]) 
+)
